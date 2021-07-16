3b653388543d43cb164411552286090f9afbfe9e
TheMulti0 <multi@codeprecise.com>
2021-07-16 13:43:31 +0300

    Removed Common project

--- Scraper.Net.Facebook/ScriptExecutor.cs
+++ Scraper.Net.Facebook/ScriptExecutor.cs
@@ -1,12 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Common
+namespace Scraper.Net.Facebook
 {
     public static class ScriptExecutor
     {
