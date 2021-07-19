2a10401c92e7aea9d17d8fa5079e3b1aa3ec42c3
TheMulti0 <multi@codeprecise.com>
2021-07-19 14:26:06 +0300

    correct youtube-dl path

--- Scraper.Net.YoutubeDl/YoutubeDlConfig.cs
+++ Scraper.Net.YoutubeDl/YoutubeDlConfig.cs
@@ -6,7 +6,12 @@ namespace Scraper.Net.YoutubeDl
     {
         public byte DegreeOfConcurrency { get; init; } = 4;
 
-        public string YoutubeDlPath { get; init; } = "youtube-dl.exe";
+        public string YoutubeDlPath { get; init; }
+#if _WINDOWS
+            = "youtube-dl.exe";
+#else
+            = "/usr/local/bin/youtube-dl";
+#endif
 
         public OptionSet OverrideOptions { get; init; }
     }
