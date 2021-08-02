2ad1e207635fe19e9fa4f37d4096aceb7db6251e
TheMulti0 <multi@codeprecise.com>
2021-08-02 13:14:05 +0300

    Updated addstream overload

--- src/Scraper.Net.Stream/ServiceCollectionExtensions.cs
+++ src/Scraper.Net.Stream/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Scraper.Net.Stream
 {
@@ -18,6 +19,22 @@ namespace Scraper.Net.Stream
                 provider => new PostsStreamer(
                     provider.GetRequiredService<IScraperService>(),
                     filter));
+        }
+
+        /// <summary>
+        /// Adds a <see cref="PostsStreamer"/>
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="action">Creates a filter to distinct between old and new posts</param>
+        /// <returns>A reference to this instance after the operation has completed</returns>
+        public static IServiceCollection AddStream(
+            this IServiceCollection services,
+            Func<IServiceProvider, PostFilter> action)
+        {
+            return services.AddSingleton(
+                provider => new PostsStreamer(
+                    provider.GetRequiredService<IScraperService>(),
+                    action(provider)));
         }
     }
 }
